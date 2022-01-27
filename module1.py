b42ed9d80fe9c8df0fc852fa63fa409c432a0063
KYIV\Kateryna_Bessonova <kateryna_bessonova@epam.com>
2022-01-27 14:28:19 +0200

    Add exceptions to module1, add draft of module2

--- module1.py
+++ module1.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import random
 # Generate 100 random numbers between 0 and 1000
 randomNumbers = random.sample(range(0, 1000), 100)
@@ -22,7 +17,7 @@ for i in range(len(randomNumbers) - 1):
 # Print sorted list
 print("The sorted list is: ", randomNumbers)
 
-# Calculate average for even and odd numbers
+# Calculate sum and counts for even and odd numbers
 oddSum = oddCount = evenSum = evenCount = 0
 for i in range(0, len(randomNumbers)):
     if randomNumbers[i] % 2 == 1:
@@ -33,5 +28,12 @@ for i in range(0, len(randomNumbers)):
         evenCount: int = evenCount + 1
 
 # print both average result in console
-print("Average of odd numbers: ", oddSum/oddCount)
-print("Average of even numbers: ", evenSum/evenCount)
+try:
+    print("Average of odd numbers: ", oddSum/oddCount)
+except ZeroDivisionError:
+    print("Average of odd numbers: ", 0)
+try:
+    print("Average of even numbers: ", evenSum/evenCount)
+except ZeroDivisionError:
+    print("Average of even numbers: ", 0)
+
